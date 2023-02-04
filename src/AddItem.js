bb17af5c482c36f14b12183562bec987a24048b6
Arda-Dinc04 <ardadinc04@gmail.com>
2023-02-03 23:03:53 -0800

    update

---
+++ src/AddItem.js
@@ -0,0 +1,16 @@
+
+export default function AddItem(){
+  
+    return(
+    <div class = "AddItem">
+      <form>
+        <label for="text-form">Type of chore: </label>
+        <input type="text" id="text-form"/>
+        <input type="button" id="" value="+"/>
+  
+      </form>
+    </div>
+    )
+  
+  
+  }
--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,5 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+//Runs testing to while using website to see eficiency etc
